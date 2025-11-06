237f32657d4b11476e1457260e5c01d27745feb1
dhruuujs <dhrubajiban78901@gmail.com>
2025-11-07 03:20:29 +0530

    Fixed typos

--- worker.js
+++ worker.js
@@ -1,14 +1,16 @@
 const defaultBlocked = ["facebook.com","instagram.com"];
 
-
-chrome.runtime.onInstalled.addEventListner(()=>{
+chrome.runtime.onInstalled.addListener(()=>{
     chrome.storage.local.set({blockedSites:defaultBlocked});
 });
 
-chrome.webNavigation.onBeforeNavigate.addEventListener(async (details)=>{
+chrome.webNavigation.onBeforeNavigate.addListener(async (details)=>{
     const url = details.url;
     if(!url.startsWith("http")) return;
     
+const {blockedSites} = await chrome.storage.local.get("blockedSites");
+if(!blockedSites) return;
+
 if(blockedSites.some(site=> url.includes(site))){
    console.log("Ayy bruda! ts is blocked", url); 
     
