13a554b4a58224fc0b3e40bbfaf19c1dba3f9f0e
dhruuujs <dhrubajiban78901@gmail.com>
2025-11-07 14:30:11 +0530

    B-02: Fixed compactibility issue

--- worker.js
+++ worker.js
@@ -2,21 +2,31 @@ const defaultBlocked = ["facebook.com","instagram.com"];
 
 chrome.runtime.onInstalled.addListener(()=>{
     chrome.storage.local.set({blockedSites:defaultBlocked});
+    console.log("Default blocked sites set:",defaultBlocked);
 });
+
+function getBlockedSites(){
+    return new Promise((resolve)=>{
+        chrome.storage.local.get("blockedSites",(result)=>{
+            resolve(result.blockedSites || []);
+        });
+    });
+}
+
 
 chrome.webNavigation.onBeforeNavigate.addListener(async (details)=>{
     const url = details.url;
     if(!url.startsWith("http")) return;
     
-const {blockedSites} = await chrome.storage.local.get("blockedSites");
+const blockedSites = await getBlockedSites();
+
 if(!blockedSites) return;
 
 if(blockedSites.some(site=> url.includes(site))){
-   console.log("Ayy bruda! ts is blocked", url); 
+   console.log("Ayy bruda! this site is blocked", url); 
     
     chrome.tabs.update(details.tabId,{
-        url: chrome.runtime.getUrl("blocked.html")
+        url: chrome.runtime.getURL("blocked.html")
     });
 }
-});
-
+},{url:[{urlMatches:"*"}] });
